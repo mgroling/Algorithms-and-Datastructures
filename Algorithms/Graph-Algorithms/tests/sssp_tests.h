8a222ef3cc3cbabf5470bd67cd33bba84d368b57
marc131183 <marc.groeling@gmail.com>
2023-05-16 13:00:49 +0200

    restructure tests to avoid redudant code

--- Algorithms/Graph-Algorithms/tests/sssp_tests.h
+++ Algorithms/Graph-Algorithms/tests/sssp_tests.h
@@ -2,15 +2,26 @@
 
 #include "../../../Datastructures/graph.h"
 #include "../sssp.h"
+#include "tuple"
 
-void test_dijkstra_startEqualsEnd() {
+std::tuple<Graph*, int, int> generate_graph_startEqualsEnd() {
     std::array<double, 9> weights = {0, 2, 0, 0, 0, 1, 1, 0, 0};
     AdjacencyMatrixGraph<3> g = AdjacencyMatrixGraph<3>(weights);
+    return std::tuple<Graph*, int, int>{&g, 0, 0};
+}
 
-    std::vector<int> path;
-    double distance;
-    std::tie(path, distance) = dijkstra(g, 0, 0);
+void assert_path_startEqualsEnd(std::vector<int> path, double distance) {
     assert(distance == 0);
     assert(path.size() == 1);
     assert(path[0] == 0);
+}
+
+void test_dijkstra_startEqualsEnd() {
+    std::vector<int> path;
+    double distance;
+    Graph* g;
+    int start, end;
+    std::tie(g, start, end) = generate_graph_startEqualsEnd();
+    std::tie(path, distance) = dijkstra(*g, start, end);
+    assert_path_startEqualsEnd(path, distance);
 }
