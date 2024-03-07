5c54d3e7d8124f4df9b3d85870912e66c23fb225
marc131183 <marc.groeling@gmail.com>
2024-03-07 16:24:54 +0100

    Popup24: Solve Homework 6 E

--- Problems/Popup24/HW6/E/main.cpp
+++ Problems/Popup24/HW6/E/main.cpp
@@ -1,51 +1,10 @@
 #include "kattisio.h"
 #include "solution.h"
 
-#include <chrono>
-
-struct Timer
-{
-    std::chrono::_V2::system_clock::time_point start;
-
-    Timer()
-    {
-        start = std::chrono::high_resolution_clock::now();
-    }
-
-    ~Timer()
-    {
-        std::cout << "Program took "
-                  << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() -
-                                                                           start)
-                         .count()
-                  << "ms to execute\n";
-    }
-};
-
 int main()
 {
-    // std::vector<std::string> input{
-    //     "3 4",
-    //     "2 3 2",
-    //     "1 1 3 2",
-    // };
-    std::vector<std::string> input;
-    std::string temp = "";
-    for (int i = 0; i < 100; i++)
-    {
-        temp += "100";
-        if (i != 99)
-        {
-            temp += " ";
-        }
-    }
-    input.push_back("100 100");
-    input.push_back(temp);
-    input.push_back(temp);
-
-    Timer timer;
-    std::pair<std::vector<int>, std::vector<int>> problem = parse_input(input);
-    bool solution = solve_problem(problem);
+    std::pair<std::vector<int>, std::vector<int>> problem = parse_input(read_input());
+    bool solution = solve_problem(problem.first, problem.second);
     std::cout << format_output(solution);
 
     return 0;
--- Problems/Popup24/HW6/E/solution.h
+++ Problems/Popup24/HW6/E/solution.h
@@ -1,58 +1,98 @@
-#include "ford_fulkerson.h"
-#include "graph.h"
+/*
+----------------------------------------------------- PROBLEM -----------------------------------------------------
+
+Binary tomography deals with the problem of reconstructing binary images from a small number of projections. One of its
+most basic problems is to construct a binary ({0, 1}-valued) matrix with given row and column sums. This is not always
+possible and your task is to determine when it is.
+
+----------------------------------------------------- INPUT -----------------------------------------------------
+
+The first line of input contains two numbers 1 <= m, n <= 1000 the number of rows and columns of the matrix. The next
+line contains m numbers 0 <= r_i <= n – the sum of each row in the matrix. The third line contains numbers 0 <= c_j <= m
+– the sum of each column in the matrix.
+
+----------------------------------------------------- OUTPUT -----------------------------------------------------
+
+Output “Yes” if there exists an m-by-n matrix, with each element either being 0 or 1, such that the sum of row i equals
+r_i for all rows and the sum of column j equals r_j for all columns. Otherwise output “No”.
+
+----------------------------------------------------- EXAMPLE -----------------------------------------------------
+
+A possible solution for sample input 1 looks like this:
+_________
+|X| |X| | 2
+| |X|X|X| 3
+| | |X|X| 2
+
+ 1 1 3 2
+
+Sample Input 1:
+3 4
+2 3 2
+1 1 3 2
+
+Sample Output 1:
+Yes
+
+Sample Input 2:
+3 3
+0 0 3
+0 0 3
+
+Sample Output 2:
+No
+
+----------------------------------------------------- SOLUTION -----------------------------------------------------
+
+The problem can be solved recursively. We can remove any row from the matrix with row sum r_i, then we have to decrement
+r_i column sums and we do that from largest to smallest. We keep doing that until either, (1) we cannot decrement enough
+columns for a row (in which case it's not posible), or (2) there are no rows left. In (2), now we only have to check if
+all column sums are zero and if yes, then it is possible.
+
+*/
+
 #include "kattisio.h"
 
-int matrix_pos_to_int(const int &row, const int &col, const int &num_cols)
+#include <algorithm>
+#include <vector>
+
+bool solve_problem_recursive(std::vector<int> &row_sums, std::vector<int> &col_sums)
 {
-    return row * num_cols + col;
+    // if there are no more rows left, we are done
+    if (!row_sums.size())
+    {
+        // check that the largest (so all) columns are zero
+        return col_sums[0] == 0;
+    }
+
+    // remove the last row
+    int last_row_sum = row_sums.back();
+    row_sums.pop_back();
+
+    // remove 1 from the last_row_sum'th largest columns
+    for (int i = 0; i < last_row_sum; i++)
+    {
+        if (col_sums[i])
+        {
+            col_sums[i]--;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    // need to sort col_sums again (could also do it in a faster way (since we only decrement the first k elements by
+    // 1), but this is fast enough anyway)
+    std::sort(col_sums.begin(), col_sums.end(), std::greater<int>());
+
+    return solve_problem_recursive(row_sums, col_sums);
 }
 
-bool solve_problem(const std::pair<std::vector<int>, std::vector<int>> problem)
+bool solve_problem(std::vector<int> &row_sums, std::vector<int> &col_sums)
 {
-    int num_rows = problem.first.size();
-    int num_cols = problem.second.size();
-    int source = 0;
-    int target = num_rows * num_cols + num_rows + num_cols + 1;
-    int offset_variables = 1;
-    int offset_equations_row_sums = offset_variables + num_rows * num_cols;
-    int offset_equations_col_sums = offset_equations_row_sums + num_rows;
+    // sort columns descendingly
+    std::sort(col_sums.begin(), col_sums.end(), std::greater<int>());
 
-    AdjacencyListGraph g = AdjacencyListGraph(num_rows * num_cols + num_rows + num_cols + 2);
-    int variable_id;
-    int all_equation_sum = 0;
-
-    for (int i = 0; i < num_rows; i++)
-    {
-        for (int j = 0; j < num_cols; j++)
-        {
-            variable_id = offset_variables + matrix_pos_to_int(i, j, num_cols);
-            // create edges from source to layer 1 (variables)
-            g.addEdge(source, variable_id, 1, false);
-            g.addEdge(variable_id, source, 0, false);
-
-            // create edges from layer 1 (variables) to layer 2 (equations)
-            // to its row sum
-            g.addEdge(variable_id, offset_equations_row_sums + i, 1, false);
-            g.addEdge(offset_equations_row_sums + i, variable_id, 0, false);
-
-            // to its col sum
-            g.addEdge(variable_id, offset_equations_col_sums + j, 1, false);
-            g.addEdge(offset_equations_col_sums + j, variable_id, 0, false);
-        }
-
-        // from row equations to target
-        g.addEdge(offset_equations_row_sums + i, target, problem.first[i], false);
-        all_equation_sum += problem.first[i];
-        g.addEdge(target, offset_equations_row_sums + i, 0, false);
-    }
-
-    for (int i = 0; i < num_cols; i++)
-    {
-        // from col equations to target
-        g.addEdge(offset_equations_col_sums + i, target, num_rows - problem.second[i], false);
-        all_equation_sum += num_rows - problem.second[i];
-        g.addEdge(target, offset_equations_col_sums + i, 0, false);
-    }
-
-    return all_equation_sum == (int)ford_fulkerson(g, source, target);
+    return solve_problem_recursive(row_sums, col_sums);
 }
