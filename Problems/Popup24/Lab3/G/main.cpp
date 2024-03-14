10a9d4b988afe71fa935242131818a5a70469d78
marc131183 <marc.groeling@gmail.com>
2024-03-14 22:21:56 +0100

    Popup24: Solve Lab3 G

--- Problems/Popup24/Lab3/G/main.cpp
+++ Problems/Popup24/Lab3/G/main.cpp
@@ -5,10 +5,7 @@
 
 int main()
 {
-    std::vector<std::string> input{
-        "9973 6", "1", "2", "3", "4", "9972", "9973",
-    };
-    std::pair<int, std::vector<int>> problem = parse_input(input);
+    std::pair<int, std::vector<int>> problem = parse_input(read_input());
     std::pair<int, std::vector<bool>> solution = solve_problem(problem);
     std::cout << format_output(solution);
 
