158663e60c065cd5c181c61b965b0140bbf5f6c7
marc131183 <marc.groeling@gmail.com>
2024-04-13 13:02:38 +0200

    solve HW9 F

--- Problems/Popup24/HW9/F/solution.h
+++ Problems/Popup24/HW9/F/solution.h
@@ -53,20 +53,94 @@ not
 Since there are only 5 variables, one can easily go through the 2^5 = 32 combinations to determine whether or not a WFF
 is true for all combinations. The real "challenge" in this problem seems to be to parse the statement efficiently.
 
-Parsing the statement can be done with a stack, we push symbols onto the stack and in certain conditions, we can pop the
-stack again:
+Parsing the statement can be easily achieved with a stack and going from right to left of the string expression. This is
+because that for any expression (K, A, N, C, E), all the variables of it are to the right and if we go from right to
+left, then once we get to that expression, its arguments are already evaluated.
 
-- if the top 2 elements on the stack are a boolean value (so a variable or a previously evaluated statement)
-- if
 
 */
 
+#include <array>
+#include <functional>
 #include <stack>
 #include <string>
+#include <unordered_map>
 #include <vector>
+
+bool evaluate_expression(const std::string &boolean_expression, const bool &p, const bool &q, const bool &r,
+                         const bool &s, const bool &t)
+{
+    std::stack<bool> variable_stack;
+    std::unordered_map<char, bool> variable_map = {{'p', p}, {'q', q}, {'r', r}, {'s', s}, {'t', t}};
+    std::unordered_map<char, std::function<bool(bool, bool)>> expression_map;
+    expression_map['K'] = [](const bool &a, const bool &b) { return a && b; };
+    expression_map['A'] = [](const bool &a, const bool &b) { return a || b; };
+    // implies function a => b equivalent to !a || b
+    expression_map['C'] = [](const bool &a, const bool &b) { return !a || b; };
+    expression_map['E'] = [](const bool &a, const bool &b) { return a == b; };
+
+    // iterate through expression in reverse order
+    for (auto it = boolean_expression.rbegin(); it != boolean_expression.rend(); it++)
+    {
+        auto ptr1 = variable_map.find(*it);
+        if (ptr1 != variable_map.end())
+        {
+            // found a variable (p, q, r, s, t), simply push it onto the stack
+            variable_stack.push(ptr1->second);
+        }
+        else
+        {
+            auto ptr2 = expression_map.find(*it);
+            if (ptr2 != expression_map.end())
+            {
+                // pop the top 2 elements of the stack, apply the function to it and put the result on the stack again
+                bool a = variable_stack.top();
+                variable_stack.pop();
+                bool b = variable_stack.top();
+                variable_stack.pop();
+                variable_stack.push(ptr2->second(a, b));
+            }
+            // symbol is the N-symbol (negation)
+            else
+            {
+                // remove the top element of the stack and replace it with the negated version
+                bool a = variable_stack.top();
+                variable_stack.pop();
+                variable_stack.push(!a);
+            }
+        }
+    }
+
+    return variable_stack.top();
+}
 
 bool solve_test_case(const std::string &test_case)
 {
+    std::array<bool, 2> values{true, false};
+
+    for (const bool &p : values)
+    {
+        for (const bool &q : values)
+        {
+            for (const bool &r : values)
+            {
+                for (const bool &s : values)
+                {
+                    for (const bool &t : values)
+                    {
+                        // if the expression is false for any variable combination, then it is not a tautology, so we
+                        // can return early
+                        if (!evaluate_expression(test_case, p, q, r, s, t))
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return true;
 }
 
 std::vector<bool> solve_problem(const std::vector<std::string> &test_cases)
