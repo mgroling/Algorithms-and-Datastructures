7b4f3be0f614b3c4a87215348e007141397d8985
marc131183 <marc.groeling@gmail.com>
2024-05-05 18:01:02 +0200

    cleanup + add != operator to Point class

--- Problems/Popup24/HW11/E/main.cpp
+++ Problems/Popup24/HW11/E/main.cpp
@@ -3,7 +3,6 @@
 
 int main()
 {
-    std::vector<std::string> input{"3 3 3", "4 4 5"};
     std::vector<std::tuple<int, int, int>> problem = parse_input(read_input());
     double solution = solve_problem(problem);
     std::cout << format_output(solution);
--- Problems/Popup24/Lab4/geometry.h
+++ Problems/Popup24/Lab4/geometry.h
@@ -46,6 +46,11 @@ template <typename T> class Point
         return x == other.x && y == other.y;
     }
 
+    bool operator!=(const Point &other) const
+    {
+        return x != other.x || y != other.y;
+    }
+
     Point operator+(const Point &other) const
     {
         return Point(x + other.x, y + other.y);
